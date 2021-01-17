4d29d746797dce0c9256a48f4c98c227055f40fc
Sergey Karpynskyi <karpinskiy94@gmail.com>
2021-01-17 13:18:34 +0200

    hw6(all)

--- js/main.js
+++ js/main.js
@@ -1,52 +1,35 @@
 import users from "./users.js"
 
 // №1
-const getUserNames = users => {
-     return users.map(user => user.name);
-};
+const getUserNames = users => users.map(user => user.name);
 
 console.log(getUserNames(users));
 
 // №2
-const getUsersWithEyeColor = (users, color) => {
-    return users.filter(user=>color===user.eyeColor);
+const getUsersWithEyeColor = (users, color) => users.filter(user=>color===user.eyeColor);
     
-};
-
 console.log(getUsersWithEyeColor(users, 'blue'));
 
 // №3
-const getUsersWithGender = (users, gender) => {
+const getUsersWithGender = (users, gender) => users.filter(user => gender === user.gender).map(user => user.name);
+
+console.log(getUsersWithGender(users, 'male'));    
     
-    const getUserName = users.filter(user => gender === user.gender);
-    return getUserName.map(user => user.name);
- 
-};
-
-console.log(getUsersWithGender(users, 'male'));
-
 // №4
-const getInactiveUsers = users => {
-  return users.filter(user=>!user.isActive)
-};
+const getInactiveUsers = users => users.filter(user => !user.isActive);
 
 console.log(getInactiveUsers(users));
 
 // №5
-const getUserWithEmail = (users, email) => {
-  return users.find(user=>user.email===email)
-};
+const getUserWithEmail = (users, email) => users.find(user=>user.email===email);
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); 
 console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
 
 // №6
-const getUsersWithAge = (users, min, max) => {
-  return users.filter(user=>user.age > min && user.age < max)
-};
+const getUsersWithAge = (users, min, max) =>  users.filter(user=>user.age > min && user.age < max);
 
 console.log(getUsersWithAge(users, 20, 30)); 
-
 console.log(getUsersWithAge(users, 30, 40));
 
 // №7
@@ -56,28 +39,21 @@ console.log(calculateTotalBalance(users));
 
 
 // №8
-const getUsersWithFriend = (users, friendName) => {
-  const getUserName = users.filter(user => user.friends.includes(friendName))
-  return getUserName.map(user => user.name);
-};
+const getUsersWithFriend = (users, friendName) =>users.filter(user => user.friends.includes(friendName)).map(user => user.name);
 
 console.log(getUsersWithFriend(users, 'Briana Decker')); 
 console.log(getUsersWithFriend(users, 'Goldie Gentry'));
 
 // №9
-const getNamesSortedByFriendsCount = users => {
-  const getUserSortedName =  users.sort((a, b) => a.friends.length - b.friends.length);
-  return getUserSortedName.map(user => user.name);
-};
+const getNamesSortedByFriendsCount = users => users.sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
 
 console.log(getNamesSortedByFriendsCount(users));
 
-
 // №10
-const getSortedUniqueSkills = users => {
-  const eew = users.map(user => user.skills);
-  
-};
+const getSortedUniqueSkills = users => users
+  .flatMap(user => user.skills)
+  .filter((user, index, arr) => arr.indexOf(user) === index)
+  .sort();  
 
 console.log(getSortedUniqueSkills(users));
 
